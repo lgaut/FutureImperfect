73609d3a5486d31156766783f39ca2f51f8dfe9f
lgaut <lgaut02@gmail.com>
2016-03-03 11:20:30 +0100

    up

---
+++ futureimperfect4spip_options.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+	 * Options du plugin FutureImperfect chargement
+ *
+ * @plugin     FutureImperfect
+ * @copyright  2016
+ * @author     Got
+ * @licence    GNU/GPL
+ * @package    SPIP\FutureImperfect\Options
+	 */
+	
+	if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+	$GLOBALS['z_blocs'] = array('content','colonne_1','head','head_js','header','footer');
